80eb479c4055bfb72a38a658abeff8e6e19baf62
Nertsal <sasha.kudasov04@gmail.com>
2022-08-25 21:30:09 +0300

    Projectile movement

--- src/model/update.rs
+++ src/model/update.rs
@@ -2,6 +2,9 @@ use super::*;
 
 impl Model {
     pub fn update(&mut self, delta_time: Time) {
-        // TODO
+        // Move projectiles
+        for projectile in &mut self.projectiles {
+            projectile.position += projectile.velocity * delta_time;
+        }
     }
 }
