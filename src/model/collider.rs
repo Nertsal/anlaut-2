fb76e73643859b43998ac7cd94559853628e02a5
Nertsal <sasha.kudasov04@gmail.com>
2022-08-26 11:04:18 +0300

    Fix collisions

--- src/model/collider.rs
+++ src/model/collider.rs
@@ -10,8 +10,8 @@ impl Collider {
     pub fn check(&self, other: &Self, delta_pos: Position) -> bool {
         match (self, other) {
             (Collider::Aabb { size: size_a }, Collider::Aabb { size: size_b }) => {
-                let a = AABB::ZERO.extend_positive(*size_a);
-                let b = AABB::point(delta_pos).extend_positive(*size_b);
+                let a = AABB::ZERO.extend_symmetric(*size_a / Coord::new(2.0));
+                let b = AABB::point(delta_pos).extend_symmetric(*size_b / Coord::new(2.0));
                 a.x_min < b.x_max && a.x_max > b.x_min && a.y_min < b.y_max && a.y_max > b.y_min
             }
         }
