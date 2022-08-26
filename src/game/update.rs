fa07e5830de5abc67cea77d5ec4070fa9a13fd27
Nertsal <sasha.kudasov04@gmail.com>
2022-08-26 20:37:04 +0300

    Camera follows the player's gun

--- src/game/update.rs
+++ src/game/update.rs
@@ -2,6 +2,15 @@ use super::*;
 
 impl Game {
     pub fn update(&mut self, delta_time: Time) {
+        let model = self.model.get();
+        if let Some(player) = model.players.get(&self.player_id) {
+            if let PlayerState::Gun { gun_id } = &player.state {
+                if let Some(gun) = model.guns.get(gun_id) {
+                    self.camera.center = gun.position;
+                }
+            }
+        }
+
         let mouse_pos = self.geng.window().mouse_pos().map(|x| x as f32);
         let mouse_pos = self
             .camera
