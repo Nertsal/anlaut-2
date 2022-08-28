4fb0c54559507498a6bc6493abcd1e25dfaed936
Nertsal <sasha.kudasov04@gmail.com>
2022-08-28 21:19:25 +0300

    Remove unused code

--- src/game/handle_event.rs
+++ src/game/handle_event.rs
@@ -2,13 +2,6 @@ use super::*;
 
 impl Game {
     pub fn handle_event(&mut self, event: geng::Event) {
-        let mouse_pos = self.geng.window().mouse_pos().map(|x| x as f32);
-        let _mouse_pos = self
-            .camera
-            .screen_to_world(self.framebuffer_size.map(|x| x as f32), mouse_pos)
-            .map(Coord::new);
-
-        #[allow(clippy::single_match)]
         match event {
             geng::Event::MouseDown { button, .. } => {
                 self.control_mode = ControlMode::Mouse;
