537f80fe1f6a2db584eaabe656858e32427fd552
Nertsal <sasha.kudasov04@gmail.com>
2022-08-28 11:24:46 +0300

    Display number of players online

--- src/game/render/ui.rs
+++ src/game/render/ui.rs
@@ -4,6 +4,8 @@ impl Game {
     pub fn draw_ui(&self, framebuffer: &mut ugli::Framebuffer) {
         let model = self.model.get();
 
+        self.draw_online(framebuffer);
+
         match &model.state {
             GameState::InProgress => {
                 if let Some(player) = model.players.get(&self.player_id) {
@@ -14,6 +16,19 @@ impl Game {
             }
             GameState::Finished { time_left } => self.draw_finished(*time_left, framebuffer),
         }
+    }
+
+    fn draw_online(&self, framebuffer: &mut ugli::Framebuffer) {
+        let screen = AABB::ZERO.extend_positive(framebuffer.size().map(|x| x as f32));
+        let font = &**self.geng.default_font();
+        draw_2d::Text::unit(
+            font,
+            format!("Players online: {}", self.model.get().players.len()),
+            Rgba::WHITE,
+        )
+        .scale_uniform(15.0)
+        .translate(screen.top_right() - vec2(170.0, 50.0))
+        .draw_2d(&self.geng, framebuffer, &geng::PixelPerfectCamera);
     }
 
     fn draw_respawn(&self, time_left: Time, framebuffer: &mut ugli::Framebuffer) {
@@ -32,14 +47,10 @@ impl Game {
     fn draw_finished(&self, time_left: Time, framebuffer: &mut ugli::Framebuffer) {
         let screen = AABB::ZERO.extend_positive(framebuffer.size().map(|x| x as f32));
         let font = &**self.geng.default_font();
-        draw_2d::Text::unit(
-            font,
-            "Game finished!",
-            Rgba::WHITE,
-        )
-        .scale_uniform(50.0)
-        .translate(screen.center() + vec2(0.0, 300.0))
-        .draw_2d(&self.geng, framebuffer, &geng::PixelPerfectCamera);
+        draw_2d::Text::unit(font, "Game finished!", Rgba::WHITE)
+            .scale_uniform(50.0)
+            .translate(screen.center() + vec2(0.0, 300.0))
+            .draw_2d(&self.geng, framebuffer, &geng::PixelPerfectCamera);
         draw_2d::Text::unit(
             font,
             format!("Restarting in {:.0}s", time_left),
