c4aee8be0b966c4f90ba87fe1939682c94e49fb1
Nertsal <sasha.kudasov04@gmail.com>
2022-08-29 16:18:35 +0300

    Fix ammo reload time

--- src/model/logic/guns.rs
+++ src/model/logic/guns.rs
@@ -5,6 +5,10 @@ impl Logic<'_> {
         let config = &self.model.assets.config;
 
         for gun in &mut self.model.guns {
+            if gun.ammo >= config.gun_magazine_size {
+                gun.next_reload = config.gun_reload_time;
+                continue;
+            }
             gun.next_reload -= self.delta_time;
             if gun.next_reload <= Time::ZERO && gun.ammo < config.gun_magazine_size {
                 gun.ammo += 1;
