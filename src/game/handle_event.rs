ba116f9dd4412ff1732ee8da7e2dd7c59a10eb3f
Nertsal <sasha.kudasov04@gmail.com>
2022-08-29 16:18:28 +0300

    Fix mouse controls

--- src/game/handle_event.rs
+++ src/game/handle_event.rs
@@ -3,15 +3,15 @@ use super::*;
 impl Game {
     pub fn handle_event(&mut self, event: geng::Event) {
         match event {
-            // geng::Event::MouseDown { button, .. } => {
-            //     self.control_mode = ControlMode::Mouse;
-            //     let heavy = match button {
-            //         geng::MouseButton::Left => false,
-            //         geng::MouseButton::Right => true,
-            //         geng::MouseButton::Middle => return,
-            //     };
-            //     self.model.send(Message::Shoot { heavy });
-            // }
+            geng::Event::MouseDown { button, .. } => {
+                self.control_mode = ControlMode::Mouse;
+                let heavy = match button {
+                    geng::MouseButton::Left => false,
+                    geng::MouseButton::Right => true,
+                    geng::MouseButton::Middle => return,
+                };
+                self.model.send(Message::Shoot { heavy });
+            }
             geng::Event::TouchStart { touches } => {
                 let touch = Touch {
                     time: self.game_time,
